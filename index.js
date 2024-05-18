3047a6072a5de316a3679ce7af48d8c7b5fffa3b
OmairSaad <alamomair724@gmail.com>
2024-05-18 18:32:00 +0530

    some changes

--- index.js
+++ index.js
@@ -20,14 +20,16 @@ try{
   bot.hears('Selection Sort', (ctx)=> {
       return ctx.reply(selectionSorting);
   })
-  bot.hears('Insertion Sorting', (ctx)=> ctx.reply(insertionSorting))
+  bot.hears('Insertion Sort', (ctx)=> ctx.reply(insertionSorting))
   bot.hears('Bubble Sort', (ctx)=> ctx.reply(bubbleSorting));
   bot.hears('Quick Sort', (ctx)=> ctx.reply(quickSorting));
   bot.hears('Merge Sort', (ctx)=> ctx.reply(mergeSorting));
-  bot.hears('Thank You', ctx=> ctx.reply('Thanks for using, 👍'));
+  bot.hears('Thank You', ctx=> ctx.reply('Thanks for using 👍'));
   bot.hears('Ok', ctx=> ctx.reply('Thank You 👍'));
   bot.hears('Okay', ctx=> ctx.reply('Thank You 👍'));
   bot.hears('Who are you', ctx=> ctx.reply('I am a telegram bot, developed by Omair Saad'))
+  bot.hears("How are you", ctx=> ctx.reply("Fine bsdk tu bata"));
+  bot.hears('Thank you', ctx=> ctx.reply('Thanks for using 👍'));
   bot.on('text', ctx=> ctx.reply("Wrong Entry, Try again dude, Warna pele jaoge 😄"));
   bot.launch();
   console.log("Running Succesfully......");
